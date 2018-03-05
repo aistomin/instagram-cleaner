16a2cd42625e5fb85fa1968d91ab5c824e7d9d12
aistomin <andrej.istomin.ikeen@gmail.com>
2018-03-05 22:09:53 +0100

    Issue #1. - fully working unit test LoginTest.java was added; - ExampleUnitTest.java was deleted.

---
+++ app/src/test/java/com/github/aistomin/incleaner/LoginTest.java
@@ -0,0 +1,17 @@
+package com.github.aistomin.incleaner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by aistomin on 05.03.18.
+ *
+ * Test that checks Instagram login.
+ */
+public final class LoginTest {
+
+    @Test
+    public void testLogin() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
